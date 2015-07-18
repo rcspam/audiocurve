81339fe2b0a807caf7799202688bda7690b4ba58
Ole-André Rodlie <olear@dracolinux.org>
2015-07-19 01:54:41 +0200

    make alwaysXY optional, other minor

--- audiocurve.cpp
+++ audiocurve.cpp
@@ -1,3 +1,31 @@
+/*
+# AudioCurve
+#
+# Copyright (c) 2015, Ole-André Rodlie <olear@fxarena.net>
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice, this
+#   list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
 #include <QCoreApplication>
 #include <QStringList>
 #include <QDebug>
@@ -19,6 +47,7 @@ int main(int argc, char *argv[])
     QString tmpfile;
     bool curveX = false;
     bool curveY = false;
+    bool forceNull = false;
     float fps = 0;
     int frames = 0;
     int heightX = 0;
@@ -80,9 +109,11 @@ int main(int argc, char *argv[])
                 heightY = args.at(i).toInt();
             foundHeightY = false;
         }
+        if (args.at(i)=="-alwaysXY")
+            forceNull = true;
     }
     if (!filename.isEmpty() && (curveX||curveY) && fps>0 && frames>0 && (heightX>0||heightY>0)) {
-        QString data = audio.dat(QDir::toNativeSeparators(filename),fps,frames,curveX,curveY,heightX,heightY);
+        QString data = audio.dat(QDir::toNativeSeparators(filename),fps,frames,curveX,curveY,heightX,heightY,forceNull);
         if (data.isEmpty()) {
             qDebug() << "Unable to generate curve data";
             return 1;
@@ -108,7 +139,7 @@ int main(int argc, char *argv[])
         }
     }
     else {
-        qDebug() << "Usage: -input audiofile -output textfile -x -y -fps 24 -frames 240 -cX 100 -cY 100";
+        qDebug() << "AudioCurve usage example: -input audiofile (-output textfile) -x (-y|-xy) -fps 24 -frames 240 -cX 100 -cY 100 (-alwaysXY)";
         return 1;
     }
     
